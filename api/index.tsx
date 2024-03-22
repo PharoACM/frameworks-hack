955e319bae105dd24583910c91dfa99a0622bb67
Jaxcoder <codenamejason@outlook.com>
2024-03-22 17:24:51 -0400

    basic layout for user interactions

--- api/index.tsx
+++ api/index.tsx
@@ -16,9 +16,11 @@ export const app = new Frog({
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 });
 
+const pharoBalance: number = 10;
+
 app.frame("/", (c) => {
-  const { buttonValue, inputText, status } = c;
-  const fruit = inputText || buttonValue;
+  const { status } = c;
+  // const rateEstimate = inputText || buttonValue;
   return c.res({
     image: (
       <div
@@ -50,19 +52,207 @@ app.frame("/", (c) => {
             whiteSpace: "pre-wrap",
           }}
         >
-          {status === "response"
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ""}`
-            : "Welcome!"}
+          {pharoBalance === 0
+            ? "You don't have any Pharo tokens."
+            : "Click next to participate."}
         </div>
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
+      pharoBalance === 0 && (
+        <Button value="participate" action="/mint">
+          Mint
+        </Button>
+      ),
+      pharoBalance > 0 && (
+        <Button value="participate" action="/participate">
+          Next
+        </Button>
+      ),
       status === "response" && <Button.Reset>Reset</Button.Reset>,
     ],
+  });
+});
+
+app.frame("/mint", (c) => {
+  const { status } = c;
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background:
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {"Mint"}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button value="submit-rate" action="/mint-tokens">
+        Mint
+      </Button>,
+      status === "response" && <Button.Reset>Changed My Mind</Button.Reset>,
+      status === "redirect" && <Button action="/participate">Next</Button>,
+    ],
+  });
+});
+
+app.frame("/participate", (c) => {
+  const { status } = c;
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background:
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {"Welcome!"}
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Enter your estimate..." />,
+      <Button value="submit-rate" action="/submit-rate">
+        Submit
+      </Button>,
+      status === "response" && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
+
+app.frame("/submit-rate", (c) => {
+  const { buttonValue, inputText, status } = c;
+  const rateEstimate = inputText || buttonValue;
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background:
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {`You submitted ${rateEstimate} hours. Thank you!`}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button value="submit-rate" action="/finish">
+        Finish
+      </Button>,
+      status === "response" && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
+
+app.frame("/finish", (c) => {
+  const { status } = c;
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background:
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {"Thank you for participating!"}
+        </div>
+      </div>
+    ),
+    intents: [],
   });
 });
 
